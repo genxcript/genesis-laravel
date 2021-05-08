21ab6670e3e12843bed1cddf5e341d3e286d74af
Emiliano Tisato <emilianotisato@gmail.com>
2021-05-07 19:51:50 -0500

    Wip

--- tests/Feature/Http/Livewire/Genesis/ResourceClassTest.php
+++ tests/Feature/Http/Livewire/Genesis/ResourceClassTest.php
@@ -5,6 +5,7 @@ namespace LaravelGenesis\Genesis\Tests\Feature\Http\Livewire\Genesis;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use LaravelGenesis\Genesis\GenesisFacade;
 use LaravelGenesis\Genesis\Tests\Fixtures\User;
+use LaravelGenesis\Genesis\Tests\Fixtures\UserResource;
 use LaravelGenesis\Genesis\Tests\TestCase;
 
 class ResourceClassTest extends TestCase
@@ -19,14 +20,64 @@ class ResourceClassTest extends TestCase
 
         $this->user = User::factory()->create();
         $this->actingAs($this->user);
-
-        // This loads the route needed in the navigation blade file
-        GenesisFacade::registerResourceRoute('LaravelGenesis\Genesis\Tests\Fixtures\UserResource');
     }
 
     /** @test */
-    public function it_renders_the_resource_columns_from_array_of_string_names()
+    public function it_renders_the_resource_table_on_route_index()
     {
+        $userResource = new UserResource();
+
+        GenesisFacade::registerResourceRoute($userResource);
+
+        User::factory([
+                'name' => 'Emiliano',
+                'email' => 'emilianotisato@gmail.com',
+            ])->create();
+
+        $response = $this->get(route('genesis::users.index'))
+        ->assertSeeLivewire('laravel-genesis.genesis.tests.fixtures.user-resource');
+
+        $response->assertSee('Emiliano');
+        $response->assertSee('emilianotisato@gmail.com');
+    }
+
+    /** @test */
+    public function it_renders_column_string_representation_if_found_in_database()
+    {
+        GenesisFacade::registerResourceRoute('LaravelGenesis\Genesis\Tests\Fixtures\UserResource');
+
+        $userResource = $this->mock(UserResource::class)->makePartial();
+
+        $userResource
+        ->shouldReceive('getRowsProperty')
+        ->andReturn([
+            'Email',
+            ]);
+
+
+        User::factory([
+            'email' => 'emilianotisato@gmail.com',
+        ])->create();
+
+        $response = $this->get(route('genesis::users.index'));
+
+        $response->assertSee('emilianotisato@gmail.com');
+    }
+
+    /** @test */
+    public function it_ignores_other_columns_that_arent_in_the_returned_array()
+    {
+        GenesisFacade::registerResourceRoute('LaravelGenesis\Genesis\Tests\Fixtures\UserResource');
+
+        $userResource = $this->mock(UserResource::class)->makePartial();
+
+        $userResource
+            ->shouldReceive('getRowsProperty')
+            ->andReturn([
+                'Email',
+                ]);
+
+
         User::factory([
                 'name' => 'Emiliano',
                 'email' => 'emilianotisato@gmail.com',
@@ -34,7 +85,7 @@ class ResourceClassTest extends TestCase
 
         $response = $this->get(route('genesis::users.index'));
 
-        $response->assertSee('Emiliano');
+        $response->assertDontSee('Emiliano');
         $response->assertSee('emilianotisato@gmail.com');
     }
 }
