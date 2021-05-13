8cd0b7cddba7c12bb6bac1afe0ad8101888362d2
Emiliano Tisato <emilianotisato@gmail.com>
2021-05-13 13:08:07 -0500

    WIP

--- src/Livewire/Traits/FormElement.php
+++ src/Livewire/Traits/FormElement.php
@@ -7,9 +7,9 @@ trait FormElement
     /**
      * The tailwind grid size
      *
-     * @var string
+     * @var int
      */
-    public int $rowSize = 1;
+    public $rowSize = 1;
 
     public function rowSize(int $size)
     {
